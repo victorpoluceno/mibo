1fb0c726f6a6a6c50b01cec0e8ee81f7a83b31ff
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2011-09-13 20:56:24 -0300

    Add initial support to offline

--- www/views.py
+++ www/views.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, send_file
 from www import app
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/static/app.manifest')
+def manifest():
+    return send_file('static/app.manifest', mimetype='text/cache-manifest')
 
